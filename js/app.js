283165b92bcac0c9b052d0307f04ed7c3e046103
Diane <diane.rebisz@gmail.com>
2017-03-23 15:52:07 -0700

    start to finish edits
    new start to finish page using same css

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 $(document).foundation();
 
+var activePart;
+
 $(document).ready(function(){
 	$('.carousel').slick({
 	  dots: true,
@@ -27,6 +29,33 @@ $(document).ready(function(){
 	    // instead of a settings object
 	  ]
 	});
+	$("#outline-sidebar").stick_in_parent();
+
+	var navChildren = $("#outline-sidebar li").children();
+    var aArray = [];
+    for (var i = 0; i < navChildren.length; i++) {
+        var aChild = navChildren[i];
+        var ahref = $(aChild).attr('href');
+        aArray.push(ahref);
+    }
+    $(window).scroll(function() {
+        var windowPos = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        var docHeight = $(document).height();
+        for (var i = 0; i < aArray.length; i++) {
+            var theID = aArray[i];
+            var secPosition = $(theID).offset().top;
+            secPosition = secPosition;
+            var divHeight = $(theID).height();
+            divHeight = divHeight + 2;
+            if (windowPos >= secPosition && windowPos < (secPosition + divHeight)) {
+                $("a[href='" + theID + "']").parent().addClass("active");
+            } else {
+                $("a[href='" + theID + "']").parent().removeClass("active");
+            }
+        }
+    });
+
 });
 
 $(".faq-each").click(function(e) {
@@ -41,4 +70,36 @@ $("nav .top-bar-right li a").click(function() {
     return false;
 });
 
+$("a.smooth-scroll").click(function() {
+    var navId= $(this).attr("href");
+    $("html body").animate({scrollTop: $(navId).offset().top+2},500);
+    $(this).addClass('active');
+    return false;
+});
+
+//countdown clock for bonus
+  $(".bonus-countdown .days").countdown("2017/03/25", function(event) {
+  	$(this).text(
+      event.strftime('%D')
+    );
+    $(this).fadeIn(100);
+  });
+  $(".bonus-countdown .hours").countdown("2017/03/25", function(event) {
+  	$(this).text(
+      event.strftime('%H')
+    );
+    $(this).fadeIn(100);
+  });
+    $(".bonus-countdown .min").countdown("2017/03/25", function(event) {
+  	$(this).text(
+      event.strftime('%M')
+    );
+    $(this).fadeIn(100);
+  });
+      $(".bonus-countdown .sec").countdown("2017/03/25", function(event) {
+  	$(this).text(
+      event.strftime('%S')
+    );
+    $(this).fadeIn(100);
+  });
 
